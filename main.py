04c431c722916c03b914c103fa793fd01e65ddd2
salivo <71031134+salivo@users.noreply.github.com>
2023-01-25 18:52:49 +0100

    Some bugs

--- main.py
+++ main.py
@@ -1,21 +1,43 @@
 from googletrans import Translator, constants
 import yaml
 
-language = "ru"
-
 translator = Translator()
 
-with open("file.yml", "r") as f:
+
+#========================================================#
+#                     Varibles                           #
+#========================================================#
+
+language = "ru"  #change language
+file_name_in = "file.yml" # input file name
+file_name_out = "output.yml" # output file name
+
+#========================================================#
+#                     Varibles                           #
+#========================================================#
+
+
+with open(file_name_in, "r") as f:
     data = yaml.load(f, Loader=yaml.FullLoader)
 
-with open("output.yml", "w") as out:
+with open(file_name_out, "w") as out:
     for key1, value1 in data.items():
         if isinstance(value1, dict):
             print(f"{key1}:")
             out.write(f"{key1}:\n")
-            #Перебір всіх ключів і значень
             for key2,value2 in value1.items():
-                if type(value2) == int or type(value2) == float:
+                if isinstance(value2, list):
+                    print(f"  {key2}:")
+                    out.write(f"  {key2}:\n")
+                    for item in value2:
+                        if type(item) != str:
+                            print(f'''    - {item}''')
+                            out.write(f'''    - {item}\n''')
+                        else:
+                            translated = translator.translate(item, dest=language)
+                            print(f'''    - "{translated.text}"''')
+                            out.write(f'''    - "{translated.text}"\n''')
+                elif type(value2) == int or type(value2) == float:
                     print(f'''  {key2}: {value2}''')
                     out.write(f'''  {key2}: {value2}\n''')
                 else:
@@ -23,7 +45,18 @@ with open("output.yml", "w") as out:
                     print(f'''  {key2}: "{translated.text}"''')
                     out.write(f'''  {key2}: "{translated.text}"\n''')
         else:
-            if type(value1) == int or type(value1) == float:
+            if isinstance(value1, list):
+                print(f"{key1}:")
+                out.write(f"{key1}:\n")
+                for item in value1:
+                    if type(item) != str:
+                        print(f'''  - {item}''')
+                        out.write(f'''  - {item}\n''')
+                    else:
+                        translated = translator.translate(item, dest=language)
+                        print(f'''  - "{translated.text}"''')
+                        out.write(f'''  - "{translated.text}"\n''')
+            elif type(value1) == int or type(value1) == float:
                 print(f'''{key1}: {value1}''')
                 out.write(f'''{key1}: {value1}\n''')
             else:
